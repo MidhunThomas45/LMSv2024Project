0a88f5e7f98ce4b5b5749216b7d29f3fec0b21a1
MidhunThomas <midhunthomas45@gmail.com>
2024-12-04 10:46:27 +0530

    -

--- LMSv2024/LMSv2024/settings.py
+++ LMSv2024/LMSv2024/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'LMSv2024.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LMS', #db name
+        'USER': 'root',
+        'PASSWORD':'12345Midhun@',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
